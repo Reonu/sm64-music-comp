ca19e6adf9c1198e8608275df5e35f2a69323643
Reonu <danileon95@gmail.com>
2021-06-21 19:11:31 +0100

    level changes

---
+++ levels/bob/texscroll.inc.h
@@ -0,0 +1,7 @@
+extern void scroll_bob_dl_fakeRoom2_mesh_layer_5_vtx_0();
+extern void scroll_bob_dl_mainRoom_mesh_layer_5_vtx_0();
+extern void scroll_bob_dl_fakeRoom1_mesh_layer_5_vtx_0();
+extern void scroll_bob_dl_Lake_mesh_layer_5_vtx_0();
+extern void scroll_bob_dl_room2_mesh_layer_5_vtx_0();
+extern void scroll_bob_dl_Waterfall_mesh_layer_5_vtx_0();
+extern void scroll_bob();
