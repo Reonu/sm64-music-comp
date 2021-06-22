c98194256ac6bc71815836d14127a184672a4c01
Reonu <danileon95@gmail.com>
2021-06-22 02:54:00 +0100

    forgot this file for the wood platform

---
+++ src/game/behaviors/wood_platform.inc.c
@@ -0,0 +1,28 @@
+#include "point_lights.h"
+
+void bhv_wood_platform_loop(void) {
+    Vec3f pos = {o->oPosX, o->oPosY + 300, o->oPosZ};
+    switch(o->oAction) {
+        case 0:
+            if (o->oTimer > o->oBehParams2ndByte) {
+                o->oAction = 1;
+            }
+            break;
+        case 1:
+            if (o->oTimer > 125) {
+                o->oAction = 2;
+                cur_obj_become_intangible();
+                cur_obj_set_model(MODEL_WOOD_PLATFORM_INTANGIBLE);
+            }
+            load_object_collision_model();
+            emit_light(pos, (o->oTimer*2), 0, 0, 0, 0, 45);
+            break;
+        case 2:
+            if (o->oTimer > 125) {
+                o->oAction = 1;
+                cur_obj_become_tangible();
+                cur_obj_set_model(MODEL_WOOD_PLATFORM);
+            }
+            break;
+    }
+}
