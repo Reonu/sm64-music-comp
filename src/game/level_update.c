9f178ae35fda414b502f5bb720c908a5d09e3da1
Reonu <danileon95@gmail.com>
2021-06-23 23:15:32 +0100

    instant warps now use the force param

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -548,32 +548,35 @@ void check_instant_warp(void) {
     }
 
     if ((floor = gMarioState->floor) != NULL) {
-        s32 index = floor->type - SURFACE_INSTANT_WARP_1B;
-        if (index >= INSTANT_WARP_INDEX_START && index < INSTANT_WARP_INDEX_STOP
-            && gCurrentArea->instantWarps != NULL) {
+        if (gMarioState->floor->type == SURFACE_INSTANT_WARP_1E) {
+            s32 index = (gMarioState->floor->force & 0xFF);
+        if (gCurrentArea->instantWarps != NULL) {
             struct InstantWarp *warp = &gCurrentArea->instantWarps[index];
 
-            if (warp->id != 0) {
-                gMarioState->pos[0] += warp->displacement[0];
-                gMarioState->pos[1] += warp->displacement[1];
-                gMarioState->pos[2] += warp->displacement[2];
+                if (warp->id != 0) {
+                    gMarioState->pos[0] += warp->displacement[0];
+                    gMarioState->pos[1] += warp->displacement[1];
+                    gMarioState->pos[2] += warp->displacement[2];
 
-                gMarioState->marioObj->oPosX = gMarioState->pos[0];
-                gMarioState->marioObj->oPosY = gMarioState->pos[1];
-                gMarioState->marioObj->oPosZ = gMarioState->pos[2];
-            #ifdef INSTANT_WARP_OFFSET_FIX
-                gMarioObject->header.gfx.pos[0] = gMarioState->pos[0];
-                gMarioObject->header.gfx.pos[1] = gMarioState->pos[1];
-                gMarioObject->header.gfx.pos[2] = gMarioState->pos[2];               
-            #endif
+                    gMarioState->marioObj->oPosX = gMarioState->pos[0];
+                    gMarioState->marioObj->oPosY = gMarioState->pos[1];
+                    gMarioState->marioObj->oPosZ = gMarioState->pos[2];
+
+                #ifdef INSTANT_WARP_OFFSET_FIX
+                    gMarioObject->header.gfx.pos[0] = gMarioState->pos[0];
+                    gMarioObject->header.gfx.pos[1] = gMarioState->pos[1];
+                    gMarioObject->header.gfx.pos[2] = gMarioState->pos[2];               
+                #endif
                 cameraAngle = gMarioState->area->camera->yaw;
 
-                change_area(warp->area);
-                gMarioState->area = gCurrentArea;
+                    change_area(warp->area);
+                    gMarioState->area = gCurrentArea;
 
-                warp_camera(warp->displacement[0], warp->displacement[1], warp->displacement[2]);
+                    warp_camera(warp->displacement[0], warp->displacement[1], warp->displacement[2]);
 
-                gMarioState->area->camera->yaw = cameraAngle;
+                    gMarioState->area->camera->yaw = cameraAngle;
+                    }
+            
             }
         }
     }
