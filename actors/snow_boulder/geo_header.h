1a0740e95dbb379f425704d4bd03f4e02147c5cc
Reonu <danileon95@gmail.com>
2021-06-23 18:07:23 +0100

    added snow boulder actor

---
+++ actors/snow_boulder/geo_header.h
@@ -0,0 +1,10 @@
+extern const GeoLayout snow_boulder_geo[];
+extern u8 snow_boulder_SUPER_MARIO_64_9D15E307_0_2_all_ci8[];
+extern u8 snow_boulder_SUPER_MARIO_64_9D15E307_0_2_all_ci8_pal_rgba16[];
+extern Vtx snow_boulder_Sphere_mesh_layer_1_vtx_cull[8];
+extern Vtx snow_boulder_Sphere_mesh_layer_1_vtx_0[85];
+extern Gfx snow_boulder_Sphere_mesh_layer_1_tri_0[];
+extern Gfx mat_snow_boulder_f3d_material[];
+extern Gfx mat_revert_snow_boulder_f3d_material[];
+extern Gfx snow_boulder_Sphere_mesh_layer_1[];
+extern Gfx snow_boulder_material_revert_render_settings[];
